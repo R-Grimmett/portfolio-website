c1ed0b61101047cd3fa26ef7add8eb6c88b87b45
R-Grimmett <grimmett.rachael@gmail.com>
2025-09-22 12:04:23 +1000

    initial theme vars, footer + navbar component

---
+++ portfolio/app/components/FooterContent.vue
@@ -0,0 +1,3 @@
+<template>
+    this is also a test
+</template>
---
+++ portfolio/app/components/NavigationContent.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-sedgwickdisplay underline">
+        This is a test
+    </h1>
+</template>
