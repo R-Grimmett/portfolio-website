464108b0a5a6445a8936d4b71454764a3812c63e
R-Grimmett <grimmett.rachael@gmail.com>
2025-09-27 13:52:27 +1000

    fresh install with nuxtV3

--- portfolio/app/components/FooterContent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    this is also a test
-</template>
--- portfolio/app/components/NavigationContent.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1 class="text-3xl font-sedgwickdisplay underline">
-        This is a test
-    </h1>
-</template>
